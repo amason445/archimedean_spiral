658818deb45d77c10177a5df861bc882393c46c4
Alex Mason <77219320+amason445@users.noreply.github.com>
2025-11-11 15:41:16 -0700

    Created Spiral Object

---
+++ SFML/ArchimedeanSpiral.h
@@ -0,0 +1,30 @@
+﻿#ifndef SPIRAL_H
+#define SPIRAL_H
+
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+class ArchimedeanSpiral {
+public:
+    explicit ArchimedeanSpiral(float b = 0.5f);
+
+    void construct_spiral(float radius, sf::Vector2f center, float a = 0.f, float dtheta = 0.01f);
+
+    const sf::VertexArray& get_vertices() const { return spiral_; }
+
+private:
+    // Archimedean Spiral -- r = a + bθ
+    // r = Radial Coordinate (distance from the center)
+    // a = Initial radius
+    // b = Pitch (growth rate)   
+    float a_ = 0.f;
+    float b_ = 0.f;
+    float theta_ = 0.f;
+    sf::VertexArray spiral_; // holds the points
+};
+
+#endif
+
+
+
+
--- SFML/main.cpp
+++ SFML/main.cpp
@@ -1,14 +1,15 @@
 ﻿#include <SFML/Graphics.hpp>
-#include <cmath>
 #include <iostream>
 #include <sstream>
 #include <string>
+
+#include "ArchimedeanSpiral.h"
 
 float input_scaling_factor() {
     float b;
     std::cout << "Please provide a scaling factor." << std::endl;
     std::cout << "This is a positive floating point integer." << std::endl;
-    std::cout << "Please enter it here: " << std::endl;
+    std::cout << "Please enter it here: ";
     std::cin >> b;
 
     if (std::cin.fail() || b <= 0.f) {
@@ -47,36 +48,11 @@ int main() {
     );
 
     auto size = window.getSize();
-    
     sf::Vector2f center{ (static_cast<float>(size.x) / 2.f), (static_cast<float>(size.x) / 2.f) };
     circle.setPosition(center);
 
-    // Archimedean Spiral -- r = a + bθ
-    // r = Radial Coordinate (distance from the center)
-    // a = Initial radius
-    // b = Pitch (growth rate)
-    const float a = 0.f;
-    float theta = 0.f;
-    float r = 0.f;
-    float x = 0.f;
-    float y = 0.f;
-    sf::VertexArray spiral(sf::PrimitiveType::LineStrip);
-
-    // The while loop works while the Radial coordinate is less than the radius of the circle in layer 1
-    // Using the formula for the radial coordinate, vectors for x and y are appended to the Vertex Array with cosine and sine
-    // You are effectively scaling sine and cosine with a constantly increasing radial coordinate
-    while (r < radius) {
-        r = a + (b * theta);
-        x = center.x + (r * std::cos(theta));
-        y = center.y + (r * std::sin(theta));
-        
-        sf::Vertex v;
-        v.position = { x, y };
-        v.color = sf::Color::Red;
-        spiral.append(v);
-
-        theta += 0.01f;
-    }
+    ArchimedeanSpiral spiral(b);
+    spiral.construct_spiral(radius, center);
 
     while (window.isOpen())
     {
@@ -88,7 +64,7 @@ int main() {
 
         window.clear();
         window.draw(circle);
-        window.draw(spiral);
+        window.draw(spiral.get_vertices());
         window.display();
     }
 }
